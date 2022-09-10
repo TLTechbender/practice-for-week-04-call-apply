22eff3170d3eb62092b43bd9e92b3f98b0f5f6bb
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-10 17:34:24 +0100

    copied: This is one tricky thing to do

--- greet.js
+++ greet.js
@@ -2,9 +2,12 @@
 function say(name) {
   console.log(`${this.message} ${name}!`);
 }
-
+/*I honestly don't know where to begin from on this project oooo*/
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+say.call(helloMessage, "John");
+say.call(heyThereMessage, "Michael");
+
+
+/* I don't fully understand this concept, but with some more problem sets, I'll gain understanding*/
